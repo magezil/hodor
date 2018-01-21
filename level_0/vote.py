ad9379b53c72248cfb20dce696629e35033792b2
magezil <256@holbertonschool.com>
2018-01-21 00:58:08 +0000

    pep8 style

--- level_0/vote.py
+++ level_0/vote.py
@@ -2,5 +2,6 @@
 """This module votes for id 256 1024 times"""
 import requests
 
+payload = {'id': '256', 'holdthedoor': 'submit'}
 for i in range(1024):
-    r = requests.post('http://158.69.76.135/level0.php', data={'id':'256', 'holdthedoor':'submit'})
+    r = requests.post('http://158.69.76.135/level0.php', data=payload)
